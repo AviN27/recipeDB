3adc0fea53c997c7525160b68b653319812c18b2
Avinash N <73270404+AviN27@users.noreply.github.com>
2023-05-25 21:20:34 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,15 +27,15 @@ const Hero = () => {
     <div className='text-white' >
         <div className={!view ? 'max-w-[800px] mx-auto text-center flex flex-col justify-center p-3 mt-40 ease-in-out duration-75' : 'fixed right-[-200%]'} >
             <p className='text-[#00df9a] font-bold p-2 md:text-5xl sm:text-3xl text-2xl md:py-5'>DON'T KNOW WHAT TO COOK TODAY?</p>
-            <p className='font-bold md:text-2xl sm:text-2xl text-1xl md:py-5'>Let an Algorithm decide for you.</p>
+            <p className='font-bold md:text-3xl sm:text-2xl text-1xl md:py-5'>Let an Algorithm decide for you.</p>
                         
         </div>
         <div className={!view ? 'max-w-[800px] mx-auto text-center flex flex-auto justify-center ease-in-out duration-75' : 'fixed right-[-200%]'}>
             <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
             onClick={fetchRecipe}>
                 <span class="font-bold relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                    <div className='font-bold md:text-3xl sm:text-2xl text-1xl'>
-                        <Typed strings={['Biriyani?' , 'Shepards Pie?' , 'Click me to find out.']} typeSpeed={120} backSpeed={140} loop/>
+                    <div className='font-bold md:text-2xl sm:text-2xl text-1xl'>
+                        <Typed strings={['Biriyani?' , 'Shepards Pie?' , 'Click to find out']} typeSpeed={120} backSpeed={140} loop/>
                     </div>
                 </span>
             </button>
